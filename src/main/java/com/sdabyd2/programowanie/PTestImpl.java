10562772ec94218131c84952c87b87fb1c10d776
WaldemarCholewinski <cholewinskii@gmail.com>
2018-01-27 15:09:46 +0100

    testy

--- src/main/java/com/sdabyd2/programowanie/PTestImpl.java
+++ src/main/java/com/sdabyd2/programowanie/PTestImpl.java
@@ -1,7 +1,7 @@
 package com.sdabyd2.programowanie;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PTestImpl {
 
@@ -10,17 +10,50 @@ public class PTestImpl {
 
     }
 
-
     public static Integer length(List<Integer> integers) {
         return integers.size();
     }
 
-    public static List<Integer> reverse(List<Integer> numbers) {
+    public static <T> List<T> reverse(List<T> numbers) {
         Collections.reverse(numbers);
         return numbers;
     }
 
-    public static Object isPalindrome(List<String> strings) {
+    public static boolean isPalindrome(List<String> list) {
+        List<String> original = new ArrayList<>(list);
+        PTestImpl.reverse(list);
+        return Objects.equals(list, original);
+    }
 
+    public static Object[] removeAt(List<String> list, int i) {
+        List<String> text = new ArrayList<>(list);
+        String kth = text.remove(i - 1);
+        return new Object[]{text, kth};
+    }
+
+    public static List<String> randomSelect(List<String> list, int elements) {
+        int listSize = list.size() - 1;
+        Random random = new Random(System.currentTimeMillis());
+        List<String> resultList = new ArrayList<String>();
+        int z = 1;
+        do {
+            resultList.add(list.get(random.nextInt(listSize)));
+            z++;
+        } while (z <= elements);
+        return resultList;
+    }
+
+    /*
+    public static <T> List<T> flatten(List<?> objects) {
+
+        return objects.stream().flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }*/
+
+    public static List<String> compress(List<String> list) {
+
+        Set<String> text = new LinkedHashSet<String>(list);
+        List<String> output = new ArrayList<>(text);
+        return output;
     }
 }
--- src/test/java/PTest.java
+++ src/test/java/PTest.java
@@ -4,46 +4,92 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
 
 public class PTest {
 
     @Test
-    public void shouldFindLastElementFromList() throws Exception{
+    public void shouldFindLastElementFromList() throws Exception {
 
-        assertThat(PTestImpl.last(asList("a","b","c","d")),is(equalTo("d")));
+        assertThat(PTestImpl.last(asList("a", "b", "c", "d")), is(equalTo("d")));
     }
+
     @Test
-    public void shouldFindListOfNonEmptyList()throws Exception{
+    public void shouldFindListOfNonEmptyList() throws Exception {
         assertThat(PTestImpl.length(
-                Arrays.asList(1,2,3,4,5)),
+                Arrays.asList(1, 2, 3, 4, 5)),
                 is(equalTo(5))
         );
     }
 
     @Test
-    public void shouldReverseAList()throws Exception{
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+    public void shouldReverseAList() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         assertThat(PTestImpl.reverse(
                 numbers),
-        is(equalTo(Arrays.asList(5,4,3,2,1))));
-    }
-    @Test
-    public void shouldReturnTrueWhenListIsPalindrome() throws Exception{
-        assertThat(PTestImpl.isPalindrome(
-                Arrays.asList("x","a","m","a","x")
-        ));
-    }
-    @Test
-    public void shouldRemoveKthElementFromList()throws Exception{
-        Object[] result = PTestImpl.removeAt(
-                Arrays.asList("a","b","c","d"),2);
-        assertThat(result[0]);
+                is(equalTo(Arrays.asList(5, 4, 3, 2, 1))));
     }
 
+    @Test
+    public void shouldReturnTrueWhenListIsPalindrome() throws Exception {
+        assertTrue(PTestImpl.isPalindrome(
+                Arrays.asList("x", "a", "m", "a", "x"))
+        );
+    }
+
+
+    @Test
+    public void shouldRemoveKthElementFromList() throws Exception {
+        Object[] result = PTestImpl.removeAt(
+                Arrays.asList("a", "b", "c", "d"), 2);
+        assertThat(result[0], equalTo(Arrays.asList("a", "c", "d")));
+        assertThat(result[1], equalTo("b"));
+    }
+
+    @Test
+    public void shouldReturnAListOfThreeRandomSelectedElements() throws Exception {
+        List<String> result =
+                PTestImpl.randomSelect(
+                        Arrays.asList(
+                                "a", "b", "c", "d", "e", "f", "g", "h"), 3);
+        System.out.println(result);
+        assertThat(result, hasSize(3));
+    }
+/*
+    @Test
+    public void shouldFlattenAListOfList() throws Exception {
+        List<String> flatten = PTestImpl.flatten(
+                asList("a", asList("b", asList("c", "d")), "e"));
+        assertThat(flatten, hasSize(5));
+        assertThat(flatten, hasItems("a", "b", "c", "d", "e"));
+    }
+    */
+
+    @Test
+    public void shouldRemoveConsecutiveDuplicatesInAList() throws Exception {
+        List<String> compressedList =
+                PTestImpl.compress(
+                        asList("a", "a", "a", "a", "b", "c", "c", "d", "e", "e", "e", "e"));
+        assertThat(compressedList, hasSize(5));
+        assertThat(compressedList, contains("a", "b", "c", "d", "e"));
+    }
+/*
+    @Test
+    public void shouldDuplicateElementsInAList() throws Exception {
+        List<String> duplicates =
+                PTestImpl.duplicate(Arrays.asList("a", "b", "c", "d"));
+        assertThat(duplicates, hasSize(8));
+        assertThat(duplicates, contains("a", "a", "b", "b", "c", "c", "d", "d"));
+    }
+*/
 }
 
